562f51940af6f69af066cda84a5bb7763f6febbd
Alex Vizcaino <avizcaino@uxland.es>
2020-06-04 10:59:11 +0200

    fix: solves issue with bounding multiple times the same store

--- src/connect.ts
+++ src/connect.ts
@@ -1,53 +1,60 @@
-import {Store, Unsubscribe} from "redux";
-import {bind} from "./bind";
-import {unbind} from "./unbind";
-import {Constructor, LitElement} from "lit-element";
-import {dedupingMixin, microTask, MixinFunction} from "@uxland/uxl-utilities";
+import { dedupingMixin, microTask, MixinFunction } from '@uxland/uxl-utilities';
+import { Constructor, LitElement } from 'lit-element';
+import { Store, Unsubscribe } from 'redux';
+import { bind } from './bind';
+import { unbind } from './unbind';
 
 export interface ConnectMixin {
-    __reduxStoreSubscriptions__: Unsubscribe[];
+  __reduxStoreSubscriptions__: Unsubscribe[];
 }
 
 export type Selector<T = any> = (state: any) => T;
 
 export interface ConnectAddOn {
-    uxlReduxWatchedProperties: { [key: string]: PropertyWatch };
-    reduxDefaultStore: Store;
-    watchProperty: (name: PropertyKey, watch: PropertyWatch) => void;
+  uxlReduxWatchedProperties: { [key: string]: PropertyWatch };
+  reduxDefaultStore: Store;
+  watchProperty: (name: PropertyKey, watch: PropertyWatch) => void;
 }
 
 export interface PropertyWatch {
-    selector: Selector;
-    store: Store;
-    name: string;
+  selector: Selector;
+  store: Store;
+  name: string;
 }
 
 export interface ConnectMixinConstructor extends LitElement {
-    new(...args: any[]): ConnectMixin & LitElement;
+  new (...args: any[]): ConnectMixin & LitElement;
 }
 
 export type ConnectMixinFunction = MixinFunction<ConnectMixinConstructor>;
 
-
-export const connect: (defaultStore?: Store<any, any>) => ConnectMixinFunction = defaultStore => dedupingMixin((superClass: Constructor<LitElement>) => {
+export const connect: (defaultStore?: Store<any, any>) => ConnectMixinFunction = (defaultStore) =>
+  dedupingMixin((superClass: Constructor<LitElement>) => {
     class connectMixin extends superClass implements ConnectMixin {
-        __reduxStoreSubscriptions__: Unsubscribe[];
+      __reduxStoreSubscriptions__: Unsubscribe[];
 
-        constructor() {
-            super();
-            microTask.run(() => bind(this));
-        }
+      private bound: boolean;
 
-        static get reduxDefaultStore(): Store | undefined {
-            return defaultStore;
-        }
+      constructor() {
+        super();
+        microTask.run(() => {
+          if (!this.bound) {
+            this.bound = true;
+            bind(this);
+          }
+        });
+      }
 
-        disconnectedCallback(): void {
-            unbind(this);
-            super.disconnectedCallback && super.disconnectedCallback();
-        }
+      static get reduxDefaultStore(): Store | undefined {
+        return defaultStore;
+      }
+
+      disconnectedCallback(): void {
+        unbind(this);
+        super.disconnectedCallback && super.disconnectedCallback();
+      }
     }
 
     return <any>connectMixin;
-});
+  });
 export default connect;
