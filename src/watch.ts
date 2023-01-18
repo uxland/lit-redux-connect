b0111486a3f5eb3f69a4b4ca33aeb92415be6dc7
DanielCabiscol <danielcabiscol@gmail.com>
2023-01-18 22:14:34 +0100

    fix: import lit decorator

--- src/watch.ts
+++ src/watch.ts
@@ -1,18 +1,14 @@
-import { PropertyDeclaration } from "lit";
-import { property } from "lit-element";
-import { always, is, isNil, lensPath, view, when } from "ramda";
-import { Store } from "redux";
-import { ConnectAddOn, Selector } from "./connect";
-import { createWatchedReduxProperty } from "./watched-redux-property";
+import {PropertyDeclaration} from 'lit';
+import {property} from 'lit/decorators.js';
+import {always, is, isNil, lensPath, view, when} from 'ramda';
+import {Store} from 'redux';
+import {ConnectAddOn, Selector} from './connect';
+import {createWatchedReduxProperty} from './watched-redux-property';
 
-const toLensSelector = (path: string) => view(lensPath(path.split(".")));
-const getSelector = (selector: Selector | string) =>
-  when(is(String), toLensSelector)(selector);
+const toLensSelector = (path: string) => view(lensPath(path.split('.')));
+const getSelector = (selector: Selector | string) => when(is(String), toLensSelector)(selector);
 const getStore = (store: Store, proto: any) =>
-  when(
-    isNil,
-    always((<ConnectAddOn>proto.constructor).reduxDefaultStore)
-  )(store);
+  when(isNil, always((<ConnectAddOn>proto.constructor).reduxDefaultStore))(store);
 
 export interface WatchOptions<> {
   store?: Store<any, any>;
@@ -31,6 +27,5 @@ export const watch =
       proto,
       String(name)
     );
-    if (proto.constructor.createProperty)
-      property(options.propertyOptions)(proto, name);
+    if (proto.constructor.createProperty) property(options.propertyOptions)(proto, name);
   };
